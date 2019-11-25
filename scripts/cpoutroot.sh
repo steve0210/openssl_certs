37e5eee7bfcd58d6b33ab6d72e2fa6e360049002
Steve Meadows <github@urwomail.me>
2019-11-25 15:50:03 -0600

    Updating scripts

--- scripts/cpoutroot.sh
+++ scripts/cpoutroot.sh
@@ -3,14 +3,21 @@
 int=${1-intermediate}
 server=$2
 ca=$(basename $(pwd))
+clientd=out/client
+serverd=out/server
 
 if [ -n "$server" ]; then
   echo "creating root.crt ..."
-  cp certs/$ca.cert.pem out/client/root.crt
+  cp certs/$ca.cert.pem $clientd/root.crt
 else
-  echo "creating root.crt, postgresql.crt, and postgresql.key"
-  cp certs/$ca.cert.pem out/server/root.crt
-  rm -f out/client/*
-  cp intermediate/certs/$int.cert.pem out/client/postgresql.crt
-  cp intermediate/private/$int.key.pem out/client/postgresql.key
+  crt=$clientd/postgresql.crt
+  key=$clientd/postgresql.key
+  pfx=$clientd/postgresql.pfx
+
+  echo "creating root.crt, postgresql.crt, and postgresql.key, postgresql.pfx"
+  cp certs/$ca.cert.pem $serverd/root.crt
+  rm -f $clientd/*
+  cp intermediate/certs/$int.cert.pem $crt
+  cp intermediate/private/$int.key.pem $key
+  cat $key $crt | openssl pkcs12 -export -out $pfx
 fi
